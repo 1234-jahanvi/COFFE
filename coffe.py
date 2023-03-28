f94b0bb710848ee76a2b33f1a1f22a3194950b49
1234-jahanvi <jahanvi.agrawal@iiitb.ac.in>
2023-03-28 22:25:58 +0530

    added files

--- coffe.py
+++ coffe.py
@@ -75,28 +75,7 @@ is_size_transistors = not args.no_sizing
 
 # Load the input architecture description file
 arch_params_dict = utils.load_arch_params(args.arch_description)
-
-"""
-
-spice_tool_select = arch_params_dict['spice_tool_select']
-
-if(spice_tool_select):
-    with open('coffe/top_level_ngspice.py', 'r') as f:
-        # Read the contents of the source file
-        source_code = f.read()
-else:
-    with open('coffe/top_level_hspice.py', 'r') as f:
-        # Read the contents of the source file
-        source_code = f.read()
     
-# Open the destination file for writing
-with open('coffe/top_level.py', 'w') as f:
-    # Write the contents of the source file to the destination file
-    f.write(source_code)
-
-"""
-    
-
 # Make the top-level spice folder if it doesn't already exist
 # if it's already there delete its content
 arch_folder = utils.create_output_dir(args.arch_description, arch_params_dict['arch_out_folder'])
